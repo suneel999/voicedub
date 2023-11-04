213531c1ccbe47bcd42cf54caaf5b5b198149551
suneel999 <suneelraju999@gmail.com>
2023-11-04 12:33:00 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import traceback
+import logging
+from flask import Flask, request, render_template, send_file
+from gradio_client import Client
+
+app = Flask(__name__)
+client = Client("https://fffiloni-videoretalking.hf.space/--replicas/8tzlv/")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        try:
+            # Get the uploaded video and audio files
+            video_file = request.files["video"]
+            audio_file = request.files["audio"]
+
+            # Save the files to the server
+            video_path = "uploads/" + video_file.filename
+            audio_path = "uploads/" + audio_file.filename
+            video_file.save(video_path)
+            audio_file.save(audio_path)
+
+            # Open the saved files as file objects
+            with open(video_path, "rb") as video_file, open(audio_path, "rb") as audio_file:
+                # Pass the file objects to the Gradio API for prediction
+                result = client.predict(
+                    {"video": video_file, "audio": audio_file},  # Dict(video: file object, audio: file object) in 'Source Video' and 'Audio Target' components
+                    api_name="/infer"
+                )
+
+            # Log the API response for debugging
+            logger.info("Gradio API Response: %s", result)
+
+            # Return the resulting video to the user
+            return send_file(result["video"], as_attachment=True)
+
+        except Exception as e:
+            # Log the error for debugging
+            traceback.print_exc()  # Print the traceback to console
+            error_message = str(e)
+            return f"An error occurred: {error_message}"
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    if not os.path.exists("uploads"):
+        os.makedirs("uploads")
+    app.run(debug=True)
